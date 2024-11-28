94d308100a83aa66d91020793f9b184d419b6f72
farshads-fall2024 <farshads@mcmaster.ca>
2024-11-27 20:00:07 -0500

    mac pls work typa commit

--- Project.cpp
+++ Project.cpp
@@ -13,8 +13,6 @@ GameMechs *mechanics_ptr;
 Player *player;
 food *food_ptr;
 
-
-
 void Initialize(void);
 void GetInput(void);
 void RunLogic(void);
@@ -23,10 +21,10 @@ void LoopDelay(void);
 void CleanUp(void);
 
 int main(void)
-{   
+{
     Initialize();
-    
-    while(mechanics_ptr->getExitFlagStatus() == false)  
+
+    while (mechanics_ptr->getExitFlagStatus() == false)
     {
         GetInput();
         RunLogic();
@@ -35,9 +33,7 @@ int main(void)
     }
 
     CleanUp();
-
 }
-
 
 void Initialize(void)
 {
@@ -49,16 +45,14 @@ void Initialize(void)
 
     objPos playerpos = player->getPlayerPos();
     food_ptr->generateFood(playerpos);
-
-
 }
 
 void GetInput(void)
 {
-   if (MacUILib_hasChar()) {
+    if (MacUILib_hasChar())
+    {
         mechanics_ptr->setInput(MacUILib_getChar());
-
-    }   
+    }
 }
 
 void RunLogic(void)
@@ -71,32 +65,29 @@ void RunLogic(void)
     int pl_y = playerpos.pos->y;
     char pl_sym = playerpos.getSymbol();
 
+    switch (input)
+    {
+    case ' ': // exit
+        mechanics_ptr->setExitTrue();
+        break;
+    case 'k':
+        food_ptr->generateFood(playerpos);
 
-    switch(input)
-    {                      
-        case ' ':  // exit
-            mechanics_ptr->setExitTrue();
-            break;
-        case 'k':
-            food_ptr->generateFood(playerpos);
-
-        default:
-            player->updatePlayerDir();
-            player->movePlayer();
-            break;
+    default:
+        player->updatePlayerDir();
+        player->movePlayer();
+        break;
     }
-    //PROCESS INPUT
-    
+    // PROCESS INPUT
 
     objPos foodPos = food_ptr->getFoodPos();
     int food_x = foodPos.pos->x;
     int food_y = foodPos.pos->y;
     char food_sym = foodPos.getSymbol();
-    
 
-    mechanics_ptr->displaychar(food_x,food_y,food_sym);
-    mechanics_ptr->displaychar(pl_x,pl_y,pl_sym);
-    
+    mechanics_ptr->displaychar(food_x, food_y, food_sym);
+    mechanics_ptr->displaychar(pl_x, pl_y, pl_sym);
+
     mechanics_ptr->clearInput();
 }
 
@@ -104,11 +95,8 @@ void DrawScreen(void)
 {
     MacUILib_clearScreen();
 
-
-    
-
     mechanics_ptr->printBoard();
-    mechanics_ptr->clearBoard();      
+    mechanics_ptr->clearBoard();
 }
 
 void LoopDelay(void)
@@ -116,14 +104,12 @@ void LoopDelay(void)
     MacUILib_Delay(DELAY_CONST); // 0.1s delay
 }
 
-
 void CleanUp(void)
 {
-    MacUILib_clearScreen();    
+    MacUILib_clearScreen();
 
     MacUILib_uninit();
     delete mechanics_ptr;
     delete player;
     delete food_ptr;
-
 }
--- food.cpp
+++ food.cpp
@@ -2,7 +2,6 @@
 #include "time.h"
 #include <stdlib.h>
 
-
 food::food(GameMechs *thisGMRef)
 {
     mainGameMechsRef = thisGMRef;
@@ -12,20 +11,20 @@ food::food(GameMechs *thisGMRef)
 
 food::~food()
 {
-
 }
 
-void food::generateFood(objPos blockOff) {
+void food::generateFood(objPos blockOff)
+{
     srand(time(NULL));
-    int i,j;
+    int i, j;
     int xRange = mainGameMechsRef->getBoardSizeX();
     int yRange = mainGameMechsRef->getBoardSizeY();
     int pos_bitV[xRange][yRange];
     int test_x;
     int test_y;
 
-    for(i=0; i < xRange; i++)
-        for(j=0; j < yRange; j++)
+    for (i = 0; i < xRange; i++)
+        for (j = 0; j < yRange; j++)
             pos_bitV[i][j] = 0;
 
     objPos player = blockOff.getObjPos();
@@ -33,22 +32,14 @@ void food::generateFood(objPos blockOff) {
     int player_y = player.pos->y;
     pos_bitV[player_x][player_y] = 1;
 
-   
-    do 
+    do
     {
-        test_x = (rand () % (xRange-3))+1;
-        test_y = (rand () % (yRange-3))+1;
-    } 
-    while (pos_bitV[test_x][test_y] == 1);
-    
-    foodPos.setObjPos(test_x,test_y,foodSym);
+        test_x = (rand() % (xRange - 3)) + 1;
+        test_y = (rand() % (yRange - 3)) + 1;
+    } while (pos_bitV[test_x][test_y] == 1);
+
+    foodPos.setObjPos(test_x, test_y, foodSym);
     pos_bitV[test_x][test_y] = 1;
-
-    
-
-
-
-
 }
 
 objPos food::getFoodPos() const
